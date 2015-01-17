35eaca28a1ff9fce51f132ffa87750e89310a8ee
Augusto Breno <augusto.breno@trt6.jus.br>
2015-01-16 23:19:06 -0300

    Classe base para suítes de testes foi renomeada.

--- qbeasy/src/test/java/org/easy/qbeasy/QBETestSuite.java
+++ qbeasy/src/test/java/org/easy/qbeasy/QBETestSuite.java
@@ -1,12 +1,12 @@
 package org.easy.qbeasy;
 
-import org.easy.testeasy.suite.BaseSuite;
+import org.easy.testeasy.suite.SuiteBase;
 
 /**
  * Suite de testes para os casos de testes da solu��o de QBE.
  * 
  * @author augusto
  */
-public class QBETestSuite extends BaseSuite {
+public class QBETestSuite extends SuiteBase {
 
 }
